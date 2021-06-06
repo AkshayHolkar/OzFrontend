be34e1aca8934a6253b745239e3a554c168cebbf
Akshay <dhamakk@gmail.com>
2021-06-07 05:02:32 +1000

    feat: add account interface

---
+++ src/app/models/account.ts
@@ -0,0 +1,13 @@
+export interface IAccount {
+    userId: string,
+    contactName: string,
+    businessName: string,
+    abn: number,
+    phone: string,
+    streetAddress: string,
+    city: string,
+    state: string,
+    postcode: number,
+    country: string,
+    user: null
+}
