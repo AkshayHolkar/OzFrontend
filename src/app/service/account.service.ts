80d4d9f9ded388258cda43651047ca562e1aab46
Akshay <dhamakk@gmail.com>
2021-06-07 08:03:47 +1000

    feat: add account service

---
+++ src/app/service/account.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IAccount } from '../models/account';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly baseURI = "https://localhost:5001/api/v1";
+
+  getaccount(): Observable<IAccount> {
+    return this.http.get<IAccount>(this.baseURI + "/Accounts");
+  }
+
+  addAccount(formData: any) {
+    return this.http.post(this.baseURI + '/Accounts', formData);
+  }
+
+  updateAccount(id: string, formData: any) {
+    return this.http.put(this.baseURI + '/Accounts/' + id, formData);
+  }
+}
