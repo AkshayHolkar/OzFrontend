365c1fbc4f6aac0954a14a8e02becea3cb69c300
Akshay <dhamakk@gmail.com>
2021-06-07 09:47:00 +1000

    feat: product interface

---
+++ src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+    id: number,
+    name: string,
+    price: number,
+    quantity: number,
+    description: string,
+    status: boolean,
+    categoryId: number,
+    sizeNotApplicable: boolean,
+    ColorNotApplicable: boolean
+}
