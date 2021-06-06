393ff97e8545182dec9929449555e9691012d9e9
Akshay <dhamakk@gmail.com>
2021-06-07 03:55:54 +1000

    refactor: consume register service in register component

--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm = this.fb.group({
+    Email: ['', [Validators.required, Validators.email]],
+    Passwords: this.fb.group({
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]
+    }, { validator: this.comparePasswords })
+  });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmP = fb.get('ConfirmPassword');
+
+    if (confirmP?.errors == null || 'passwordMissMatch' in confirmP.errors) {
+      if (fb.get('Password')?.value != confirmP?.value) {
+        confirmP?.setErrors({ passwordMissMatch: true });
+      } else {
+        confirmP?.setErrors(null);
+      }
+    }
+  }
+
+  isFail = false;
+
+  constructor(public service: UserService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.service.register(this.registerForm.value).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigateByUrl("/user/profile");
+      },
+      err => {
+        if (err.status == 400) {
+          this.isFail = true;
+        }
+      }
+    );
+  }
 }
