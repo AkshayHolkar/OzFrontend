dc09d785f06da5298cddd78c8527d79aa66b954f
Akshay <dhamakk@gmail.com>
2021-06-07 16:04:33 +1000

    feat: add color service

---
+++ src/app/service/color.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IColor } from '../models/color';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly baseURI = "https://localhost:5001/api/v1";
+
+  getColors(productId: number): Observable<IColor[]> {
+    return this.http.get<IColor[]>(this.baseURI + "/Colours?productId=" + productId);
+  }
+
+  addColor(formData: any) {
+    return this.http.post(this.baseURI + '/Colours', formData);
+  }
+
+  updateColor(id: number, formData: any) {
+    return this.http.put(this.baseURI + '/Colours/' + id, formData);
+  }
+}
