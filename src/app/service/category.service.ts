a029e0571490aca4656c3a1d91027a5fd91be747
Akshay <dhamakk@gmail.com>
2021-06-07 11:30:43 +1000

    feat: add category service

---
+++ src/app/service/category.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICategory } from '../models/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly baseURI = "https://localhost:5001/api/v1";
+
+  getCategories(): Observable<ICategory[]> {
+    return this.http.get<ICategory[]>(this.baseURI + "/Categories");
+  }
+}
