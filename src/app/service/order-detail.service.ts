b641a5f70b6312fd2c349f8c26a6ca230bf19b0b
Akshay <dhamakk@gmail.com>
2021-06-10 13:16:09 +1000

    feat: add order detail service

---
+++ src/app/service/order-detail.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IOrderDetail } from '../models/orderDetail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly baseURI = "https://localhost:5001/api/v1";
+
+  getOrderDetail(orderId: number): Observable<IOrderDetail[]> {
+    return this.http.get<IOrderDetail[]>(this.baseURI + "/OrderDetails?orderId=" + orderId);
+  }
+
+  addOrderDetail(formData: any) {
+    return this.http.post(this.baseURI + '/OrderDetails', formData);
+  }
+}
