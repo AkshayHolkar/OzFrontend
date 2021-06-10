a0f2c3ff083270bf9014708a3c6830fc9468322c
Akshay <dhamakk@gmail.com>
2021-06-10 15:56:13 +1000

    refactor: consume orderDetail service in orderDetail component

--- src/app/components/dashboard/order-detail/order-detail.component.ts
+++ src/app/components/dashboard/order-detail/order-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IOrderDetail } from 'src/app/models/orderDetail';
+import { OrderDetailService } from 'src/app/service/order-detail.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-order-detail',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailComponent implements OnInit {
 
-  constructor() { }
+  orderId = 0;
+  total = 0;
+  orderDetail: IOrderDetail[] = [];
+
+  constructor(private orderDetailService: OrderDetailService, private route: ActivatedRoute, private _location: Location) { }
 
   ngOnInit(): void {
+    this.getOrderDetail();
   }
 
+  getOrderDetail() {
+
+    this.route.params.subscribe(
+      (params) => {
+        this.orderId = params['id'];
+      });
+
+    this.orderDetailService.getOrderDetail(this.orderId).subscribe(
+      result => {
+        this.orderDetail = result;
+        this.getTotal();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTotal() {
+    for (let o of this.orderDetail) {
+      this.total += o.totalPrice;
+    }
+  }
+
+  backClicked() {
+    this._location.back();
+  }
 }
