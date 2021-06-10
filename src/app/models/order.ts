9bd2701745c3370d8bb437d938bb0a75b04536e3
Akshay <dhamakk@gmail.com>
2021-06-10 13:00:48 +1000

    feat: add order interface

---
+++ src/app/models/order.ts
@@ -0,0 +1,9 @@
+import { IOrderDetail } from "./orderDetail";
+
+export interface IOrder {
+    id?: number,
+    dateCreation: Date,
+    customerId?: string,
+    orderStatus?: string,
+    orderDetails?: IOrderDetail[]
+}
