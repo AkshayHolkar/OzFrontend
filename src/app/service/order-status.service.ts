b545c5182367a0fdb3f098072142382075b4b571
Akshay <dhamakk@gmail.com>
2021-06-10 16:20:10 +1000

    feat: add order status service

---
+++ src/app/service/order-status.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IOrderStatus } from '../models/orderStatus';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderStatusService {
+
+  constructor(private http: HttpClient) { }
+
+  readonly baseURI = "https://localhost:5001/api/v1";
+
+  getOrderStatus(): Observable<IOrderStatus[]> {
+    return this.http.get<IOrderStatus[]>(this.baseURI + "/OrderStatuses");
+  }
+}
