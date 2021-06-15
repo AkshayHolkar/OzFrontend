ab98d03f9de7e40fbef9ec9b18158ae4dcf25f17
Akshay <dhamakk@gmail.com>
2021-06-15 15:59:24 +1000

    refactor: consume cart service  in cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ICart } from 'src/app/models/cart';
+import { IOrder } from 'src/app/models/order';
+import { IOrderDetail } from 'src/app/models/orderDetail';
+import { CartService } from 'src/app/service/cart.service';
+import { OrderDetailService } from 'src/app/service/order-detail.service';
+import { OrderService } from 'src/app/service/order.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +14,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  carts: ICart[] = [];
+  total: number = 0;
 
-  ngOnInit(): void {
+  newOrder: IOrder = {
+    dateCreation: new Date()
   }
 
+  newOrderDetail: IOrderDetail = {
+    orderId: 0,
+    productId: 0,
+    productName: '',
+    quantity: 0,
+    totalPrice: 0
+  }
+
+  constructor(private cartService: CartService, private orderService: OrderService, private orderDetailService: OrderDetailService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getCarts();
+  }
+
+  getCarts() {
+    this.cartService.getcarts().subscribe(
+      (result) => {
+        this.carts = result;
+        this.getTotal();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTotal() {
+    for (let cart of this.carts) {
+      this.total += cart.price * cart.quantity;
+    }
+  }
+
+  order() {
+    this.orderService.addOrder(this.newOrder).subscribe(
+      (result: any) => {
+        this.addOrderDetail(result.id);
+        this.router.navigate(["user/success"]);
+      },
+      error => {
+
+      }
+    );
+  }
+
+  addOrderDetail(id: number) {
+
+    for (let cart of this.carts) {
+      this.newOrderDetail.orderId = id;
+      this.newOrderDetail.productId = cart.productId;
+      this.newOrderDetail.productName = cart.productName;
+      this.newOrderDetail.color = cart.color;
+      this.newOrderDetail.size = cart.size;
+      this.newOrderDetail.quantity = cart.quantity;
+      this.newOrderDetail.totalPrice = cart.quantity * cart.price;
+
+      this.orderDetailService.addOrderDetail(this.newOrderDetail).subscribe(
+        result => {
+          this.removeCart(cart.id || 0);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  removeCart(id: number) {
+    this.cartService.deleteCart(id).subscribe(
+      result => { },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
