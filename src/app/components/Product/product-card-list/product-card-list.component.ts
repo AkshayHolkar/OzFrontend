18f85f1e389ff11020995c1c212ade8e0ea7ea34
Akshay <dhamakk@gmail.com>
2021-06-15 13:44:49 +1000

    refactor: consume get all products service in product cart list

--- src/app/components/Product/product-card-list/product-card-list.component.ts
+++ src/app/components/Product/product-card-list/product-card-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/models/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-product-card-list',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardListComponent implements OnInit {
 
-  constructor() { }
+  products: IProduct[] = [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts() {
+
+    this.productService.getProducts().subscribe(
+      (products) => {
+        this.products = products;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
