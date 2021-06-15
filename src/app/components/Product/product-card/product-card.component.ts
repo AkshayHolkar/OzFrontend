f43921cac1c360d1d48faf1c1e948485cc6de107
Akshay <dhamakk@gmail.com>
2021-06-15 13:55:00 +1000

    refactor: add dynamic content to product card component

--- src/app/components/Product/product-card/product-card.component.ts
+++ src/app/components/Product/product-card/product-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IImage } from 'src/app/models/image';
+import { IProduct } from 'src/app/models/product';
+import { ImageService } from 'src/app/service/image.service';
 
 @Component({
   selector: 'app-product-card',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() product: IProduct = {
+    name: '',
+    price: 0,
+    quantity: 0,
+    description: '',
+    status: false,
+    categoryId: 0,
+    sizeNotApplicable: false,
+    colorNotApplicable: false
+  };
+
+  url = '';
+
+  constructor(private imageService: ImageService) { }
 
   ngOnInit(): void {
+    this.getFetureImage();
   }
 
+  getFetureImage() {
+    this.imageService.getFetureImage(this.product.id || 0).subscribe(
+      (result: IImage) => {
+        console.log(result);
+        this.url = result.imageScr || '';
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
