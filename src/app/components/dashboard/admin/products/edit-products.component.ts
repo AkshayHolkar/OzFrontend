0a84c3a3c29dcf5ee121e39a72c8b1b4311db08b
Akshay <dhamakk@gmail.com>
2021-07-01 12:29:26 +1000

    feat: edit products component

---
+++ src/app/components/dashboard/admin/products/edit-products.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IProduct } from 'src/app/models/product';
+import { ProductService } from 'src/app/service/product.service';
+
+@Component({
+  selector: 'app-edit-products',
+  templateUrl: './edit-products.component.html',
+  styleUrls: ['./edit-products.component.scss']
+})
+export class EditProductsComponent implements OnInit {
+
+  products: IProduct[] = [];
+  isSuccess = false;
+
+  constructor(private router: Router, private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(
+      result => {
+        this.products = result;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  productDetail(id: number) {
+    this.router.navigate(['product', id]);
+  }
+
+  onSubmit(id: number) {
+    this.productService.removeProduct(id).subscribe(
+      result => {
+        this.isSuccess = true;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+}
