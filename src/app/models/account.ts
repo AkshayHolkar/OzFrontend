c96ed8b1398d61be3e13a128e3ac306cd01714b3
Akshay <dhamakk@gmail.com>
2021-07-02 10:16:03 +1000

    refactor: updated IAccount interface

--- src/app/models/account.ts
+++ src/app/models/account.ts
@@ -1,13 +1,14 @@
 export interface IAccount {
-    userId: string,
-    contactName: string,
-    businessName: string,
-    abn: number,
-    phone: string,
-    streetAddress: string,
-    city: string,
-    state: string,
-    postcode: number,
-    country: string,
-    user?: null
+  userId: string,
+  contactName: string,
+  businessName: string,
+  abn: number,
+  phone: string,
+  streetAddress: string,
+  city: string,
+  state: string,
+  postcode: number,
+  country: string,
+  user?: null,
+  approved?: boolean
 }
